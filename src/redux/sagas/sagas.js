2251fcc2b61ab3182cb4151db085495880c4072b
Eazylaykzy <eazylaykzy@gmail.com>
2020-01-03 08:06:02 -0800

    Request mode set to "no-cors"

--- src/redux/sagas/sagas.js
+++ src/redux/sagas/sagas.js
@@ -49,7 +49,7 @@ const getWeather = async (query) => {
 
 	const resp = await fetch(`${url}?${URLparams}`, {
 		method: "GET", // *GET, POST, PUT, DELETE, etc.
-		mode: "cors", // no-cors, *cors, same-origin
+		mode: "no-cors", // no-cors, *cors, same-origin
 		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
 		credentials: "same-origin", // include, *same-origin, omit
 		headers: {
